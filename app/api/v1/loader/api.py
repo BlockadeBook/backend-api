06c5e03eda7dc12c0cb43673eca1fc41222270ae
Icy-13 <zorinms@mail.ru>
2025-05-18 21:36:06 +0300

    formatted loader api and added readme

--- app/api/v1/loader/api.py
+++ app/api/v1/loader/api.py
@@ -4,14 +4,17 @@ from app.loader.loader import load_json
 
 loader_api = Blueprint("loader_api", __name__)
 
+
 @loader_api.route('/load', methods=['POST'])
 def load():
-  """loads given json to database"""
-  data = request.json
-  res = ""
-  def log(msg):
-    nonlocal res
-    res += msg
-  load_json(data, log)
-  print(res)
-  return res
+    """loads given json to database"""
+    data = request.json
+    res = ""
+
+    def log(msg):
+        nonlocal res
+        res += msg
+
+    load_json(data, log)
+    print(res)
+    return res
