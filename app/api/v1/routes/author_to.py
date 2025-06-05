59a7c0f9abc8621e1081d94e5cd12bfd4044fa76
Egr <egortuisov@gmail.com>
2025-06-06 00:51:14 +0300

    get all authors and single author endpoints

--- app/api/v1/routes/author_to.py
+++ app/api/v1/routes/author_to.py
@@ -3,10 +3,9 @@ import httpx
 
 author_to_api = Blueprint("author_to_api", __name__, url_prefix='/author')
 
-
-def _author_get(author_id: int) -> tuple[dict | None, int, Exception | None]:
+def _get_json(url_in_db) -> tuple[dict | None, int, Exception | None]:
     db_url = current_app.config['DATABASE_URL']
-    full_url = f"{db_url}/authors/{author_id}"
+    full_url = f"{db_url}/{url_in_db}"
     try:
         response = httpx.get(full_url, timeout=10)
         response.raise_for_status()
@@ -16,6 +15,13 @@ def _author_get(author_id: int) -> tuple[dict | None, int, Exception | None]:
     except httpx.HTTPStatusError as exc:
         return None, exc.response.status_code, exc
 
+def _author_get(author_id: int) -> tuple[dict | None, int, Exception | None]:
+    return _get_json(f"authors/{author_id}")
+
+def return_response(data, status_code, error):
+    if not data:
+        return make_response(jsonify({"error": str(error)}), status_code)
+    return make_response(jsonify(data), 200)
 
 def _author_to_field(field: str, author_id: int):
     data, status_code, error = _author_get(author_id)
@@ -23,6 +29,12 @@ def _author_to_field(field: str, author_id: int):
         return make_response(jsonify({"error": str(error)}), status_code)
     return make_response(jsonify(data[field]), 200)
 
+def get_request(url: str):
+    return return_response(*_get_json(url))
+
+@author_to_api.route('/<int:author_id>', methods=['GET'])
+def single_author(author_id: int):
+    return get_request(f"authors/{author_id}")
 
 @author_to_api.route('/social_classes/<int:author_id>', methods=['GET'])
 def author_to_social_classes(author_id: int):
@@ -72,6 +84,9 @@ def family_status_id_to_name(family_status_id: int):
     except httpx.HTTPStatusError as exc:
         return None, exc.response.status_code, exc
 
+@author_to_api.route('/all', methods=['GET'])
+def all_authors():
+    return get_request("authors/")
 
 @author_to_api.route('/greet', methods=['GET'])
 def greeting():
